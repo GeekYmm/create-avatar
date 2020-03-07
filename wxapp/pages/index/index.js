03d2c2534c2a069b5a2a6692a8f6c757eb0d7957
GeekYmm <geekreading@163.com>
2020-03-07 10:20:01 +0800

    init: wxapp

---
+++ wxapp/pages/index/index.js
@@ -0,0 +1,86 @@
+// pages/index/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        funtionList: [{
+            title: "头像戴口罩",
+            icon: "",
+            description: "为武汉加油💪，给头像戴口罩😷",
+            hasOpen: false
+        }, {
+            title: "头像戴圣诞帽",
+            icon: "",
+            description: "圣诞节🎄，给头像圣诞帽",
+            hasOpen: true
+        }, {
+            title: "新春祝福头像",
+            icon: "",
+            description: "喜迎春节🧨，用头像表达祝福",
+            hasOpen: true
+        }, {
+            title: "抖音热门标签头像",
+            icon: "",
+            description: "新生代最爱的标签头像🤩",
+            hasOpen: true
+        }]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
